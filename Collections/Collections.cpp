c0fa3ab99e11897cd36389f282246707d76b78e9
Saahil Agrawal <saahil0906@outlook.com>
2018-12-27 14:37:01 -0700

    initial update

--- Collections/Collections.cpp
+++ Collections/Collections.cpp
@@ -3,10 +3,84 @@
 
 #include "pch.h"
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm> //for sort and count
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	vector<int> vi;
+	for (int i = 1; i <= 10; i++)
+	{
+		vi.push_back(i);
+	}
+
+	// iterates through every item in vector vi and prints each "item" of vector with space between
+	for (auto item : vi)
+	{
+		cout << item << " ";
+	}
+
+	cout << "\n";
+
+	vector<string> vs;
+
+	cout << "Enter three words with spaces in between: ";
+	for (int i = 0; i < 3; i++)
+	{
+		string s;
+		cin >> s;
+		vs.push_back(s);
+	}
+
+	for (auto item : vs)
+	{
+		cout << item << " ";
+	}
+
+	cout << "\n";
+
+	cout << "int vector vi has " << vi.size() << " elements.\n";
+
+	vi[5] = 3;
+	vi[6] = -1;
+	vi[1] = 99;
+
+	for (auto item : vi)
+	{
+		cout << item << " ";
+	}
+	cout << "\n";
+
+	for (unsigned int i = 0; i < vi.size(); i++)
+	{
+		cout << vi[i] << " ";
+	}
+	cout << "\n";
+
+	for (auto i = begin(vi); i != end(vi); i++)
+	{
+		cout << *i << " ";
+	}
+	cout << "\n";
+
+	sort(begin(vs), end(vs));
+	for (auto item : vs)
+	{
+		cout << item << " ";
+	}
+	cout << "\n";
+
+	int threes = count(begin(vi), end(vi), 3);
+	cout << "vector of ints has " << threes << " elements containing \"3\"\n";
+	int tees = count(begin(vs[0]), end(vs[0]), 't');
+	cout << "vector vs has " << tees << " letter t's";
+
+
+	return 0;
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
